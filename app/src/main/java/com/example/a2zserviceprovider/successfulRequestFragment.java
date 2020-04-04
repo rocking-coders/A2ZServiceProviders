0f45bd5363b125e7d21bf922565a61c3785d43c2
Saniya-Gupta <cu.18bcs2293@gmail.com>
2020-04-04 13:39:57 +0600

    AC Repair Update UI

--- app/src/main/java/com/example/a2zserviceprovider/successfulRequestFragment.java
+++ app/src/main/java/com/example/a2zserviceprovider/successfulRequestFragment.java
@@ -1,12 +1,16 @@
 package com.example.a2zserviceprovider;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +20,7 @@ public class successfulRequestFragment extends Fragment {
 
     Context context;
 
+    ImageButton imgButton;
     public successfulRequestFragment(Context context){
         this.context = context;
     }
@@ -25,13 +30,22 @@ public class successfulRequestFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_successful_request, container, false);
         TextView textView = root.findViewById(R.id.TVsuccessfulRequest);
-        textView.setText("Your Request has been accepted. Technician will contact You.");
+        textView.setText("Your Request has been accepted. Technician will contact you soon.");
 
         //removing service location from services Data
         SharedPreferences sharedPreferences = context.getSharedPreferences("services Data", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove("serviceLocation");
         editor.apply();
+
+        imgButton =(ImageButton)root.findViewById(R.id.home);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+            }
+        });
 
         return root;
     }
